8c7755cd4eb851ceec6fb1c1f52eea9f9bd42051
MAfreen3502 <mullaafreen999@gmail.com>
2026-01-28 09:28:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const heroText = document.querySelector(".hero h1");
+
+heroText.style.opacity = 0;
+
+setTimeout(() => {
+  heroText.style.transition = "1s";
+  heroText.style.opacity = 1;
+}, 500);
+const images = document.querySelectorAll(".gallery img");
+const lightbox = document.getElementById("lightbox");
+const lightImg = document.getElementById("lightbox-img");
+
+images.forEach(img=>{
+  img.addEventListener("click", ()=>{
+    lightbox.style.display = "flex";
+    lightImg.src = img.src;
+  });
+});
+
+lightbox.addEventListener("click", ()=>{
+  lightbox.style.display = "none";
+});
